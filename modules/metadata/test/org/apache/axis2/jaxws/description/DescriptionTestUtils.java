b6153c8b62129c19da6586f904c5120ee1936b1c
Thorsten Schöning <tschoening@users.noreply.github.com>
2017-08-22 15:37:06 +0200

    AXIS2-5793: toURL is deprecated and breaks with paths containing spaces.

--- modules/metadata/test/org/apache/axis2/jaxws/description/DescriptionTestUtils.java
+++ modules/metadata/test/org/apache/axis2/jaxws/description/DescriptionTestUtils.java
@@ -31,7 +31,7 @@ import java.net.URL;
 import java.io.File;
 
 /**
- * 
+ *
  */
 public class DescriptionTestUtils {
 
@@ -45,7 +45,7 @@ public class DescriptionTestUtils {
         return getWSDLURL("WSDLTests.wsdl");
 
     }
-    
+
     static public String getWSDLLocation(String wsdlFileName) {
         String basedir = System.getProperty("basedir", ".");
         String urlString = basedir + "/test-resources/wsdl/" + wsdlFileName;
@@ -57,7 +57,7 @@ public class DescriptionTestUtils {
         String urlString = getWSDLLocation(wsdlFileName);
         // Get the URL to the WSDL file.  Note that 'basedir' is setup by Maven
         try {
-            wsdlURL = new File(urlString).getAbsoluteFile().toURL();
+            wsdlURL = new File(urlString).getAbsoluteFile().toURI().toURL();
         } catch (Exception e) {
             System.out.println("Caught exception creating WSDL URL :" + urlString +
                     "; exception: " + e.toString());
@@ -104,7 +104,7 @@ public class DescriptionTestUtils {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-        
+
         return returnComposite;
     }
 
